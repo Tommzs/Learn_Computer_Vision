3e752206d1ef2127ffdeb67d860f6c92926e844c
Tomas Novak <tomasnovak@Tomass-MacBook-Pro.local>
2019-08-18 22:04:08 +0200

    Project 1: Template matching using OpenCV done

--- week1/detect_object/template_match.py
+++ week1/detect_object/template_match.py
@@ -3,6 +3,17 @@ import numpy as np
 from matplotlib import pyplot as plt
 import argparse
 
+def match_template(image, template):
+    w, h = template.shape[::-1]
+    res = cv.matchTemplate(image,template,cv.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    cv.rectangle(image,top_left, bottom_right, 0, 5)
+    plt.imshow(image,cmap = 'gray')
+    plt.show()
+
+
 def read_args():
     parser = argparse.ArgumentParser(description="""Find object in image by template""")
     parser.add_argument(
@@ -14,8 +25,9 @@ def read_args():
     args = vars(parser.parse_args())
     return args["image"], args["template"]
 
+
 if __name__ == "__main__":
     im_path, tp_path = read_args()
     img = cv.imread(im_path, 0)
     tmp = cv.imread(tp_path, 0)
-    plt.imshow(img)
+    match_template(img, tmp)
